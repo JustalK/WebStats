c3dc3ab7d36c1ca308ff01efd3b07a6aadfc6582
Kevin Justal <kevin.justal@etu.unice.fr>
2016-01-19 23:31:04 +0100

    [5] Adding all the informations for the graph

--- statistique.php
+++ statistique.php
@@ -1,11 +1,27 @@
 <br /><br />
 <div class="row block-bot-content">
-	<div id="graph" class="col-md-12 vertical-align">
-			<canvas id="LatsujA" width="auto" height="500px">
-				<canvas id="LatsujB"></canvas>
+	<div id="graph" class="col-md-12" style="position: relative;background: #FFFFFF;height:500px;">
+			<canvas id="Grille" style="position: absolute;z-index: 1;">
+			</canvas>
+			
+			<canvas id="Player1" style="position: absolute;z-index: 2;">
+			</canvas>
+			
+			<canvas id="Player2" style="position: absolute;z-index: 3;">
+			</canvas>
+			
+			<canvas id="Player3" style="position: absolute;z-index: 4;">
+			</canvas>
+			
+			<canvas id="Player4" style="position: absolute;z-index: 5;">
+			</canvas>
+			
+			<canvas id="LatsujA" style="position: absolute;z-index: 6;">
+			</canvas>
+			<canvas id="LatsujB" style="position: absolute;z-index: 7;">
+				Your browser does not support the HTML5 canvas tag.
 			</canvas>
 	</div>
 </div>
-  	
   	
 C'est <?php echo $_POST["JOUEUR"]; ?> !
